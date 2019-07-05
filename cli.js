29a8f87a344be30e8c591a7c5391a61496af226e
Miguel Mota <hello@miguelmota.com>
2019-07-05 15:05:50 -0700

    Fix spacing

--- cli.js
+++ cli.js
@@ -18,8 +18,7 @@ const cli = meow(`
       --silent, -s Silent output
 
     Examples
-    $ eth_send --from 4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d --to 0xffcf8fdee72ac11b5c542428b35eef5769c409f0
-      --amount 0.01 --network rinkeby --silent
+    $ eth_send --from 4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d --to 0xffcf8fdee72ac11b5c542428b35eef5769c409f0 --amount 0.01 --network rinkeby --silent
 
     0x8ee7ed489c7cb206cd9b4ff65a5d2977324b4f727b12cd2e0c0bbcaa59219e00
 `, {
